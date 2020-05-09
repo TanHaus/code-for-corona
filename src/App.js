a88d12169975d7e2feec227f4197a5289db55567
Thien Tran <gau.nernst@yahoo.com.sg>
2020-05-09 15:13:11 +0800

    layout

--- src/App.js
+++ src/App.js
@@ -11,18 +11,20 @@ function App() {
     <div>
       <ul>
         <li><a href="/entry">Entry</a></li>
-        <li><a href="/configure">Configure</a></li>
-        <li><a href="/start-day">Start-day</a></li>
+        <li><a href="/choose-activities">Activities</a></li>
+        <li><a href="/personalize">Personalization</a></li>
+        <li><a href="/start-day">NewDay</a></li>
       </ul>
     
     <Router>
       <Switch>
         <Route path="/entry" component={EntryPoint} />
         <Route path="/choose-activities" component={Activities} />
-        <Route path="/pesronalize" component={Personalization} />
+        <Route path="/personalize" component={Personalization} />
         <Route path="/start-day" component={NewDay} />
       </Switch>
     </Router>
+
     </div>
   );
 }
--- src/pages/Personalization.jsx
+++ src/pages/Personalization.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import { Grid, Button, Fade } from "@material-ui/core";
+import { Card, CardContent, CardActionArea } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    root: {
+        flexGrow: 1,
+    },
+    item: {
+        // padding: '20px auto',
+        margin: '20px auto',
+    },
+    square: {
+        
+    }
+})
 
 function Personalization(props) {
+    const classes = useStyles();
     return (
         <Fade in="true">
         <Grid 
@@ -11,24 +27,70 @@ function Personalization(props) {
             justify="center"
             alignItems="center"
         >   
-            <Grid 
-                item 
-                xs
-                container
-                direction="column"
-                justify="center"
-                alignItems="center"
-            >
-                <h1>Choose your activities</h1>
-                <p>as many as you like</p>
+            <Grid item className={classes.item}>
+                <h1>Personalize your activities</h1>
             </Grid>
-            <Grid item xs>
-                <Button component={Link} to="/start-day">Let's get started</Button>
+            <Grid item className={classes.item}>
+                <h2>I want to cook ....</h2>
+                <LearnOption text="Asian cuisine"></LearnOption>
+                <LearnOption text="Western cuisine"></LearnOption>
+                <LearnOption text="Anything. Surprise me!"></LearnOption>
+            </Grid>
+            <Grid item className={classes.item}>
+                <h2>Dishes that I want to cook are similar to ...</h2>
+                <Grid container space={5}>
+                    <SquareOption text="Pho"></SquareOption>
+                    <SquareOption text="Kway Teow"></SquareOption>
+                    <SquareOption text="Bibimbap"></SquareOption>
+                </Grid>
+            </Grid>
+            <Grid item className={classes.item}>
+                <h2>I want to learn ....</h2>
+                <LearnOption text="Science & Mathematics"></LearnOption>
+                <LearnOption text="Arts & Humanities"></LearnOption>
+                <LearnOption text="Anything. Surprise me!"></LearnOption>
+            </Grid>
+            <Grid item className={classes.item}>
+                <h2>Dishes that I want to cook are similar to ...</h2>
+                <Grid container space={5}>
+                    <SquareOption text="Web Development"></SquareOption>
+                    <SquareOption text="Contemporary Art"></SquareOption>
+                    <SquareOption text="Microeconomics"></SquareOption>
+                </Grid>
+            </Grid>
+            <Grid item className={classes.item}>
+                <Button component={Link} to="/start-day">Confirm</Button>
             </Grid>
             
         </Grid>
         </Fade>
     );
+}
+
+function LearnOption(props) {
+
+    return (
+        <Card>
+            <CardActionArea>
+            <CardContent>
+            <p>{props.text}</p>
+            </CardContent>
+            </CardActionArea>
+        </Card>
+    )
+}
+
+function SquareOption(props) {
+
+    return (
+        <Card>
+            <CardActionArea>
+            <CardContent>
+            <p>{props.text}</p>
+            </CardContent>
+            </CardActionArea>
+        </Card>
+    )
 }
 
 export default Personalization;
