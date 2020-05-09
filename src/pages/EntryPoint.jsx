b51d09a55f387aea277f007b0ea8d19071e04b41
Thien Tran <gau.nernst@yahoo.com.sg>
2020-05-09 13:41:25 +0800

    Update EntryPoint.jsx

--- src/pages/EntryPoint.jsx
+++ src/pages/EntryPoint.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Grid, Button, Fade } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
- 
+
+
 
 function EntryPoint(props) {
     return (
