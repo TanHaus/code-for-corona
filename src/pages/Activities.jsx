c62478c15f8eff915a9f7c136bab0ecaf517482e
Dan Tran <picasdan99@gmail.com>
2020-05-09 16:53:28 +0800

    update

--- src/pages/Activities.jsx
+++ src/pages/Activities.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-import { makeStyles, Fade, Grid, Card, CardActionArea, CardMedia, CardContent, Typography, ButtonBase } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { makeStyles, Fade, Grid, Card, CardActionArea, CardMedia, CardContent, ButtonBase, Button } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
-    body: {
-      padding: theme.spacing(5),
-      paddingTop: theme.spacing(3)
+    item: {
+        margin: '20px auto',
     },
     card: {
       minWidth: 340,
-      maxWidth: 380
+      maxWidth: 380,
+      margin: '20px auto',
     },
     buttonBase: {
         position: 'relative',
@@ -25,14 +26,14 @@ const useStyles = makeStyles(theme => ({
 
 function ActivityCard(props) {
     const classes = useStyles();
-    const { activity, setActivity } = props;
+    const { name, pic, activity, setActivity } = props;
     return (
         <Card
         container
         xs={12}
         className={classes.card}
         style={{
-            backgroundColor: activity ? "#FFFFFF" : "#AAAAAA"
+            backgroundColor: activity ? "#AAAAAA" : "#FFFFFF"
         }}
     >
         <ButtonBase 
@@ -43,10 +44,10 @@ function ActivityCard(props) {
             <CardActionArea>
                 <CardMedia 
                     component="img"
-                    alt="Cook a dish"
+                    alt={name}
                     height="140"
-                    image="../static/images/cook-a-dish"
-                    title="Cook a dish"
+                    image={pic}
+                    title={name}
                 />
                 <CardContent>
                     <Grid
@@ -55,16 +56,11 @@ function ActivityCard(props) {
                         justify="center"
                         alignItems="center"
                     >
-                        <Typography
-                            gutterBottom
-                            variant="h6"
-                            component="h3"
-                            style={{
-                                color: activity ? "#000000" : "#FFFFFF"
-                            }}
-                        >
-                            Cook a dish
-                        </Typography>
+                        <p style={{
+                            color: activity ? "#FFFFFF" : "#000000" 
+                        }}>
+                            {name}
+                        </p>
                     </Grid>
                 </CardContent>
             </CardActionArea>
@@ -72,9 +68,30 @@ function ActivityCard(props) {
     </Card>
     )
 }
+
 function Activities() {
     const classes = useStyles();
-    const [cooking, setCooking] = useState(false);
+    const [cookDish, setCookDish] = useState(false);
+    const [watchMovie, setWatchMovie] = useState(false);
+    const [readBook, setReadBook] = useState(false);
+    const [workOut, setWorkOut] = useState(false);
+    const [learnOnline, setLearnOnline] = useState(false);
+    const [cleanRoom, setCleanRoom] = useState(false);
+    const [chatFnF, setChatFnF] = useState(false);
+    const [donate, setDonate] = useState(false);
+    const [volunteer, setVolunteer] = useState(false);
+
+    const handleSubmitForm = () => {
+        localStorage.setItem("cookDish", JSON.stringify(cookDish));
+        localStorage.setItem("watchMovie", JSON.stringify(watchMovie));
+        localStorage.setItem("readBook", JSON.stringify(readBook));
+        localStorage.setItem("workOut", JSON.stringify(workOut));
+        localStorage.setItem("learnOnline", JSON.stringify(learnOnline));
+        localStorage.setItem("cleanRoom", JSON.stringify(cleanRoom));
+        localStorage.setItem("chatFnF", JSON.stringify(chatFnF));
+        localStorage.setItem("donate", JSON.stringify(donate));
+        localStorage.setItem("volunteer", JSON.stringify(volunteer));
+    }
 
     return (
         <Fade in="true">
@@ -84,42 +101,38 @@ function Activities() {
             justify="center"
             alignItems="center"
         >   
-            <Grid 
-                xs={12}
-                container
-                direction="column"
-                justify="center"
-                alignItems="center"
-            >
+            <Grid item className={classes.item}>
                 <h1>Choose your activities</h1>
-                <p>as many as you like</p>
+                <p>as many as you like</p> 
             </Grid>
-            <Grid>
-                <Grid
-                    xs={12}
-                    container
-                    direction="column"
-                    justify="center"
-                    alignItems="center"
-                >
-                    <Typography
-                        variant="h4"
-                        component="h2"
-                    >
-                        Leisure
-                    </Typography>
-                </Grid>
-                <Grid
-                    xs={12}
-                    container
-                    direction="column"
-                    justify="center"
-                    alignItems="center"
-                >
-                    <ActivityCard activity={cooking} setActivity={setCooking} />
-                </Grid>
+            <Grid item className={classes.item}>
+                <h2>Leisure</h2>
+                <ActivityCard name="Cook a dish" activity={cookDish} setActivity={setCookDish} />
+                <ActivityCard name="Watch a movie" activity={watchMovie} setActivity={setWatchMovie} />
+                <ActivityCard name="Read a book" activity={readBook} setActivity={setReadBook} />
             </Grid>
-            
+            <Grid item className={classes.item}>
+                <h2>Self-improvement</h2>
+                <ActivityCard name="Do a workout" activity={workOut} setActivity={setWorkOut} />
+                <ActivityCard name="Take a free online course" activity={learnOnline} setActivity={setLearnOnline} />
+                <ActivityCard name="Clean your room" activity={cleanRoom} setActivity={setCleanRoom} />
+            </Grid>
+            <Grid item className={classes.item}>
+                <h2>Human Interaction</h2>
+                <ActivityCard name="Have a chat with family and friends" activity={chatFnF} setActivity={setChatFnF} />
+                <ActivityCard name="Make a donation" activity={donate} setActivity={setDonate} />
+                <ActivityCard name="Volunteer" activity={volunteer} setActivity={setVolunteer} />
+            </Grid>
+            <Grid item className={classes.item}>
+                <Button
+                    variant="outlined"
+                    onClick={() => handleSubmitForm()}
+                    component={Link} 
+                    to="/personalize"
+                >
+                        Continue
+                </Button>
+            </Grid>
         </Grid>
         </Fade>
     )
--- src/pages/Personalization.jsx
+++ src/pages/Personalization.jsx
@@ -19,6 +19,16 @@ const useStyles = makeStyles({
 
 function Personalization(props) {
     const classes = useStyles();
+    const cookDish = JSON.parse(localStorage.getItem("cookDish"));
+    const watchMovie = JSON.parse(localStorage.getItem("watchMovie"));
+    const readBook = JSON.parse(localStorage.getItem("readBook"));
+    const workOut = JSON.parse(localStorage.getItem("workOut"));
+    const learnOnline = JSON.parse(localStorage.getItem("learnOnline"));
+    const cleanRoom = JSON.parse(localStorage.getItem("cleanRoom"));
+    const chatFnF = JSON.parse(localStorage.getItem("chatFnF"));
+    const donate = JSON.parse(localStorage.getItem("donate"));
+    const volunteer = JSON.parse(localStorage.getItem("volunteer"));
+
     return (
         <Fade in="true">
         <Grid 
@@ -30,34 +40,40 @@ function Personalization(props) {
             <Grid item className={classes.item}>
                 <h1>Personalize your activities</h1>
             </Grid>
-            <Grid item className={classes.item}>
-                <h2>I want to cook ....</h2>
-                <LearnOption text="Asian cuisine"></LearnOption>
-                <LearnOption text="Western cuisine"></LearnOption>
-                <LearnOption text="Anything. Surprise me!"></LearnOption>
-            </Grid>
-            <Grid item className={classes.item}>
-                <h2>Dishes that I want to cook are similar to ...</h2>
-                <Grid container space={5}>
-                    <SquareOption text="Pho"></SquareOption>
-                    <SquareOption text="Kway Teow"></SquareOption>
-                    <SquareOption text="Bibimbap"></SquareOption>
+            {cookDish ? <div>
+                <Grid item className={classes.item}>
+                    <h2>I want to cook ....</h2>
+                    <LearnOption text="Asian cuisine"></LearnOption>
+                    <LearnOption text="Western cuisine"></LearnOption>
+                    <LearnOption text="Anything. Surprise me!"></LearnOption>
                 </Grid>
-            </Grid>
-            <Grid item className={classes.item}>
-                <h2>I want to learn ....</h2>
-                <LearnOption text="Science & Mathematics"></LearnOption>
-                <LearnOption text="Arts & Humanities"></LearnOption>
-                <LearnOption text="Anything. Surprise me!"></LearnOption>
-            </Grid>
-            <Grid item className={classes.item}>
-                <h2>Dishes that I want to cook are similar to ...</h2>
-                <Grid container space={5}>
-                    <SquareOption text="Web Development"></SquareOption>
-                    <SquareOption text="Contemporary Art"></SquareOption>
-                    <SquareOption text="Microeconomics"></SquareOption>
+                <Grid item className={classes.item}>
+                    <h2>Dishes that I want to cook are similar to ...</h2>
+                    <Grid container space={5}>
+                        <SquareOption text="Pho"></SquareOption>
+                        <SquareOption text="Kway Teow"></SquareOption>
+                        <SquareOption text="Bibimbap"></SquareOption>
+                    </Grid>
                 </Grid>
-            </Grid>
+            </div> : <div />
+            }
+            {learnOnline ? <div>
+                <Grid item className={classes.item}>
+                    <h2>I want to learn ....</h2>
+                    <LearnOption text="Science & Mathematics"></LearnOption>
+                    <LearnOption text="Arts & Humanities"></LearnOption>
+                    <LearnOption text="Anything. Surprise me!"></LearnOption>
+                </Grid>
+                <Grid item className={classes.item}>
+                    <h2>Dishes that I want to cook are similar to ...</h2>
+                    <Grid container space={5}>
+                        <SquareOption text="Web Development"></SquareOption>
+                        <SquareOption text="Contemporary Art"></SquareOption>
+                        <SquareOption text="Microeconomics"></SquareOption>
+                    </Grid>
+                </Grid>
+            </div> : <div />
+            }
             <Grid item className={classes.item}>
                 <Button component={Link} to="/start-day">Confirm</Button>
             </Grid>
