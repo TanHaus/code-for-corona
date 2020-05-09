8f975fe368ac934638f3bc53f6eb4ae957c3ef22
Thien Tran <gau.nernst@yahoo.com.sg>
2020-05-09 13:15:10 +0800

    add layout

--- src/pages/EntryPoint.jsx
+++ src/pages/EntryPoint.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Grid, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
  
+
 function EntryPoint(props) {
     return (
-        <Grid >
-            <h1>Seeking to be inspired during Circuit Breaker?</h1>
-            <Button >Haro</Button>
+        <Grid 
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+        >   
+            <Grid item xs>
+                <h1>Seeking to be inspired during Circuit Breaker?</h1>
+            </Grid>
+            <Grid item xs>
+                <Button component={Link} to="/configure">Let's get started</Button>
+            </Grid>
+            
         </Grid>
     );
 }
--- src/pages/Personalization.jsx
+++ src/pages/Personalization.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { Grid, Button } from "@material-ui/core";
 
 function Personalization(props) {
     return (
-        <p>This is the page to personalize</p>
+        <Grid 
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+        >   
+            <Grid item xs>
+                <h1>Choose your activities</h1>
+                <p>as many as you like</p>
+            </Grid>
+            <Grid item xs>
+                <Button >Let's get started</Button>
+            </Grid>
+            
+        </Grid>
     );
 }
 
