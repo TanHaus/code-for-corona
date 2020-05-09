64f811d8588bf79bd8995cadf74fd435421d2246
Thien Tran <gau.nernst@yahoo.com.sg>
2020-05-09 12:51:06 +0800

    Update EntryPoint.jsx

--- src/pages/EntryPoint.jsx
+++ src/pages/EntryPoint.jsx
@@ -1,8 +1,13 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import { Grid, Button } from "@material-ui/core";
+ 
 function EntryPoint(props) {
     return (
-        <p>This is the entry point</p>
+        <Grid >
+            <h1>Seeking to be inspired during Circuit Breaker?</h1>
+            <Button >Haro</Button>
+        </Grid>
     );
 }
 
